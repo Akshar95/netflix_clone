d238f979bd70a9915544daf94b938c0fa60f4153
Akshar95 <patel.akshar295@gmail.com>
2022-10-06 11:24:07 +0100

    initial set up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCR7sKdtEOjO994yMWRqtsunmG2Co7ZAIM",
+  authDomain: "netflix-clone-987e0.firebaseapp.com",
+  projectId: "netflix-clone-987e0",
+  storageBucket: "netflix-clone-987e0.appspot.com",
+  messagingSenderId: "460756306213",
+  appId: "1:460756306213:web:3dd7d47bf0e125b57b32cc",
+};
